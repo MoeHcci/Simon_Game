d396746d473cf03db918265b5f3e1c1c28489108
Mohamd Imad <medoimad@hotmail.com>
2022-08-23 22:23:48 -0400

    -Generate a random number -based on the random number make a var that selects an item from a list based on the random number -relate the button to the random number -fade out then fade in the related button -add sound everytime the fade out and fade in button is clicked

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var colors = ["green", "red", "yellow", "blue"];
+
+function nextIteration() {
+var randomNumber = Math.floor(Math.random() * (4 - 0) );
+return randomNumber;
+};
+
+var randomColor = colors[nextIteration()];
+
+var pattern = [randomColor][0];
+
+var idPattern = ('#'+pattern);
+
+
+// The jQuery code inside the ready keyword. this is helpful, because it will
+// execute the code once jQuery has been loaded properly The .ready() method offers a way to run
+// JavaScript code as soon as the page's Document Object Model (DOM) becomes safe to manipulate.
+// We can use $ or jQuery to call out jQuery instead of the lengthy Villa JS
+$(document).ready(function() {
+
+  $(idPattern).fadeOut(100).fadeIn(100);
+  $(idPattern).click(function() {
+  var audio = new Audio("fastSmallSweepTransitionSound.mp3");
+  audio.play();
+});
+
+
+});
