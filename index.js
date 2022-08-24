eea7e2cc72581d5b2e12eec9af57e9b99c9932d4
Mohamd Imad <medoimad@hotmail.com>
2022-08-24 08:47:11 -0400

    capture the data when one of the buttons is clicked using jQuery. Then, u need to find a way to store the data

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// Added this here so we do not get an error at the begining
+var selectedColorByUser;
+
 
 var colors = ["green", "red", "yellow", "blue"];
 
 function nextIteration() {
-var randomNumber = Math.floor(Math.random() * (4 - 0) );
-return randomNumber;
+  var randomNumber = Math.floor(Math.random() * (4 - 0));
+  return randomNumber;
 };
 
 var randomColor = colors[nextIteration()];
 
 var pattern = [randomColor][0];
 
-var idPattern = ('#'+pattern);
+var idPattern = ('#' + pattern);
+
+var clickedPattern = [];
 
 
 // The jQuery code inside the ready keyword. this is helpful, because it will
@@ -21,9 +26,25 @@ $(document).ready(function() {
 
   $(idPattern).fadeOut(100).fadeIn(100);
   $(idPattern).click(function() {
-  var audio = new Audio("fastSmallSweepTransitionSound.mp3");
-  audio.play();
-});
+    var audio = new Audio("fastSmallSweepTransitionSound.mp3");
+    audio.play();
+  });
+
+
+  $(".game_buttons").click(function() {
+     selectedColorByUser = this.id;
+
+  })
+;
+
+
+
+
+
+
+
+
+
 
 
 });
