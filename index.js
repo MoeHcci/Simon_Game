497c750d4d900e9c85d38926411f0f73b24b11ca
Mohamd Imad <medoimad@hotmail.com>
2022-08-25 07:30:12 -0400

    download 4 sounds

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ var selectedColorByUser;
 
 var colors = ["green", "red", "yellow", "blue"];
 
+// The random # generator
 function nextIteration() {
   var randomNumber = Math.floor(Math.random() * (4 - 0));
   return randomNumber;
 };
-
+// Breakdown of codes to produce an ID of one of the colors
 var randomColor = colors[nextIteration()];
-
 var pattern = [randomColor][0];
-
 var idPattern = ('#' + pattern);
 
 // introduce an empty list
 var clickedPattern = [];
 
+// Function that plays sounds
+function soundClicked() {
+  var audio = new Audio("fastSmallSweepTransitionSound.mp3");
+  audio.play();
+}
 
 // The jQuery code inside the ready keyword. this is helpful, because it will
 // execute the code once jQuery has been loaded properly The .ready() method offers a way to run
@@ -25,16 +29,18 @@ var clickedPattern = [];
 // We can use $ or jQuery to call out jQuery instead of the lengthy Villa JS
 $(document).ready(function() {
 
+  // A button is animated once the page is loaded based on whatever idPattern value is
   $(idPattern).fadeOut(100).fadeIn(100);
-  $(idPattern).click(function() {
-    var audio = new Audio("fastSmallSweepTransitionSound.mp3");
-    audio.play();
-  });
+  // A Sound is made for whatever idPattern the value is
+  $(idPattern).click(soundClicked);
 
-// The ID of the pushed button is added to the empty list each time putting is pressed
+
+  // The ID of the pushed button is added to the empty list each time putting is pressed
   $(".game_buttons").click(function() {
     selectedColorByUser = this.id
     clickedPattern.push(this.id)
+    // var audio_1 = new Audio("fastSmallSweepTransitionSound.mp3")
+    // audio_1.play()
   });
 
 });
